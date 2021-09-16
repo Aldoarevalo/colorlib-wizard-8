20ffe2a96dfd104e8461a17830b024c6b0950a58
Miguel Villalba <mike.mavc27@gmail.com>
2021-09-16 18:51:09 -0400

    ajuste de jquery

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 $serverName = "ALDO\MSSQLSERVER2";  
 try  
 {  
-$connect = new PDO( "sqlsrv:server=$serverName ; Database=testing", "zigo", "1234");  
-//$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
+//$connect = new PDO( "sqlsrv:server=$serverName ; Database=testing", "zigo", "1234");  
+$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
 $connect->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
 echo 'conexion exitosa';
 }  
